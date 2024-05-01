14d6135233b92d664c50d9c42afa8f01c59a1466
HossamAhmedGitHub <hossam_a_ali@yahoo.com>
2024-05-01 15:41:44 +0300

    create verifyElementVisibility

--- src/test/java/Utilities/Utility.java
+++ src/test/java/Utilities/Utility.java
@@ -5,5 +5,14 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
 public class Utility {
-
+    public static boolean verifyElementVisibility(WebDriver driver, By Locator){
+        Long duration = 0L;
+        try {
+            WaitsUtils.waitUntilElementVisible(driver,Locator,duration);
+            return true;
+        }catch (TimeoutException e){
+            System.out.println("Time's out for the element to be visible");
+            return false;
+        }
+    }//end method verifyElementVisible
 }
